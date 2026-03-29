82f4f0f6a28d2491d5172340f05c8358ddc91a15
Barry Brown <barryjbro@gmail.com>
2026-03-28 20:30:49 -0500

    Add top bars

--- app/src/main/java/com/example/koinedictionary/MainActivity.kt
+++ app/src/main/java/com/example/koinedictionary/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -47,6 +49,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
     var selectedTabIndex by remember { mutableIntStateOf(0) }
@@ -54,6 +57,11 @@ fun MainScreen() {
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = { Text(text = tabs[selectedTabIndex]) }
+            )
+        },
         bottomBar = {
             TabRow(
                 selectedTabIndex = selectedTabIndex,
