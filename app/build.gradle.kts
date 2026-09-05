a5f718a8ad289501142659640b140a60cb290104
Barry Brown <barryjbro@gmail.com>
2026-09-05 16:57:55 -0500

    up compilesdk and gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.whoisbarry.pocketgreekdictionary"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.whoisbarry.pocketgreekdictionary"
